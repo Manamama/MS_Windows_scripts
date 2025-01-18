3a2de6a3401050a165ea3b9fc8a054a7936ab38f
Ma Mana ma Manama <78492008+Manamama@users.noreply.github.com>
2025-01-18 11:30:26 +0100

    Update Unix.sh

--- Unix.sh
+++ Unix.sh
@@ -1,13 +1,11 @@
 #First things first if e.g. booting off ISO file
-sudo apt install clang
-sudo apt install lobomp-dev
+
 sudo apt install cpufetch
-sudo apt intall neofetch
+sudo apt install neofetch
 cpufetch
 neofetch -off
-sudo apt install git
-sudo apt install cmake
-sudo apt install clang
+sudo apt install git cmake clang -y 
+sudo apt install lobomp-dev -y 
 
 cd Downloads
 git clone https://github.com/Dr-noob/peakperf
