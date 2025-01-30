1ab11a2db6ba28679b510c75ca61918bd7cf618d
Ma Mana ma Manama <78492008+Manamama@users.noreply.github.com>
2025-01-30 10:58:49 +0100

    Update Unix.sh

--- Unix.sh
+++ Unix.sh
@@ -7,6 +7,13 @@ neofetch --off
 sudo apt install git cmake clang -y 
 sudo apt install lobomp-dev -y 
 
+
+#Install clang-18
+bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+
+#Install OpenMP 18, for llama.cpp etc.:
+sudo apt install libomp-18-dev -y 
+
 cd Downloads
 git clone https://github.com/Dr-noob/peakperf
 #In CmakeList.txt comment out #Set (SANITY_FLAGS)
